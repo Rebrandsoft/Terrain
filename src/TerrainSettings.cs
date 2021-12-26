b9a5a9c7adc540684ccc2c1a2175a40dc48ae1a1
Rebrandsoft <rebrandsoft@gmail.com>
2021-12-26 18:57:27 +0000

    New Plugin

---
+++ src/TerrainSettings.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows.Forms;
+using ExileCore.Shared.Attributes;
+using ExileCore.Shared.Interfaces;
+using ExileCore.Shared.Nodes;
+
+namespace Terrain
+{
+	class TerrainSettings : ISettings
+	{
+		public ColorNode TerrainColor { get; set; } = new ColorNode(0x9F9F9F4D);
+		public ToggleNode Enable { get; set; } = new ToggleNode(false);
+		[Menu("Hotkey")] public HotkeyNode HotKey { get; set; } = Keys.Oem5;
+	}
+}
